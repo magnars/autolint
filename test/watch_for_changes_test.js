c1cd8fbb6bec24ed0ba410b46cf7a82fc4ca5dba
Magnar Sveen <magnars@gmail.com>
2011-05-31 16:19:24 +0200

    initial commit

---
+++ test/watch_for_changes_test.js
@@ -0,0 +1,13 @@
+/*jslint indent: 2, onevar: false, undef: true */
+/*globals module, require */
+
+require('sinon-nodeunit');
+var testCase = require('sinon-nodeunit/deps/nodeunit').testCase;
+var watchForChanges = require('watch_for_changes');
+
+module.exports = testCase({
+  "should be function": function (test) {
+    
+    test.done();
+  },
+});
