ebd6069c787390c9f549bbb8d552dd39aec68336
Magnar Sveen <magnars@gmail.com>
2011-06-01 06:11:33 +0200

    updated to buster test framework

--- test/watch_for_changes_test.js
+++ test/watch_for_changes_test.js
@@ -1,13 +1,42 @@
 /*jslint indent: 2, onevar: false, undef: true */
 /*globals module, require */
 
-require('sinon-nodeunit');
-var testCase = require('sinon-nodeunit/deps/nodeunit').testCase;
+var buster = require("buster");
+var assert = buster.assert;
 var watchForChanges = require('watch_for_changes');
+var fs = require('fs');
 
-module.exports = testCase({
-  "should be function": function (test) {
-    
-    test.done();
+var today = 1306852705933;
+
+buster.testCase("watchForChanges", {
+  setUp: function () {
+    this.stub(fs, 'watchFile');
   },
+  
+  "should be function": function () {
+    assert.isFunction(watchForChanges);
+  },
+  
+  "should use fs.watchFile": function () {
+    watchForChanges('file.txt');
+    assert.calledWith(fs.watchFile, 'file.txt');
+  },
+  
+  "should callback when changed": function () {
+    fs.watchFile.yields({mtime: new Date(today)}, {mtime: new Date(today + 3)});
+    var changed = this.stub();
+
+    watchForChanges('', changed);
+    
+    assert.called(changed);
+  },
+  
+  "should ignore when mtime is equal": function () {
+    fs.watchFile.yields({mtime: new Date(today)}, {mtime: new Date(today)});
+    var changed = this.stub();
+
+    watchForChanges('', changed);
+    
+    assert.notCalled(changed);
+  }
 });
