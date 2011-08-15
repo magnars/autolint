0b302fad488defe5b2c94dcc1a594f02caf10b40
Magnar Sveen <magnars@gmail.com>
2011-08-15 07:56:44 +0200

    moved pluralize to lib

---
+++ test/pluralize-test.js
@@ -0,0 +1,17 @@
+/*jslint indent: 2, onevar: false */
+/*global require */
+
+var buster = require('buster');
+var assert = buster.assert;
+
+var pluralize = require('pluralize');
+
+buster.testCase("pluralize", {
+  "should not pluralize one item": function () {
+    assert.equals(pluralize(1, "item"), "1 item");
+  },
+  
+  "should pluralize several items": function () {
+    assert.equals(pluralize(2, "item"), "2 items");
+  }
+});
