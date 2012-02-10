462c746a5e73560c7e0209906e80dfb3ef6dd315
Magnar Sveen <magnars@gmail.com>
2012-02-11 00:49:46 +0100

    Create default config file.

--- test/configuration-test.js
+++ test/configuration-test.js
@@ -1,4 +1,8 @@
 var buster = require("buster");
+
+var fs = require("fs");
+var util = require("util");
+
 var configuration = require("../lib/configuration");
 
 buster.testCase('Configuration', {
@@ -20,5 +24,27 @@ buster.testCase('Configuration', {
 
   "should overwrite defaults from autolint.js": function () {
     assert.match(configuration.load(), { linterOptions: { indent: 2 } });
+  },
+
+  "should not overwrite existing config file": function () {
+    assert.exception(function () {
+      configuration.createDefaultConfigFile();
+    });
+  },
+
+  "should do lots of mumbo jumbo to copy the blank config file": function () {
+    var oldFile = {};
+    var newFile = { once: this.stub().yields() };
+
+    this.stub(configuration, "exists").returns(false);
+    this.stub(fs, "createReadStream").returns(oldFile);
+    this.stub(fs, "createWriteStream").returns(newFile);
+    this.stub(util, "pump");
+    
+    configuration.createDefaultConfigFile();
+
+    assert.calledOnceWith(fs.createWriteStream, "./autolint.js");
+    assert.calledOnce(fs.createReadStream); // jeez, __dirname for the lose
+    assert.calledOnceWith(util.pump, oldFile, newFile);
   }
 });
