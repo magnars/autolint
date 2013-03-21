170a9664102951985d7750b52f0c90fcf9cd9d70
Magnar Sveen <magnars@gmail.com>
2013-03-21 13:01:35 +0100

    Some renamings for clarity.

--- test/watch-for-changes-test.js
+++ test/watch-for-changes-test.js
@@ -2,9 +2,9 @@ var buster = require('buster');
 var EventEmitter = require('events').EventEmitter;
 var fs = require("fs");
 
-var watchForLint = require('../lib/watch-for-lint');
+var watchForChanges = require('../lib/watch-for-changes');
 
-buster.testCase("watchForLint", {
+buster.testCase("watchForChanges", {
   setUp: function () {
     this.linter = { checkFile: this.stub() };
     this.stub(fs, 'watch');
@@ -12,14 +12,14 @@ buster.testCase("watchForLint", {
   },
 
   "should register new files to be watched": function () {
-    watchForLint(this.repository, this.linter);
+    watchForChanges(this.repository, this.linter);
     this.repository.emit('newFile', {name: 'file1.js'});
     assert.calledOnce(fs.watch);
     assert.calledWith(fs.watch, 'file1.js');
   },
 
   "should check changed files for lint": function () {
-    watchForLint(this.repository, this.linter);
+    watchForChanges(this.repository, this.linter);
     fs.watch.yields('change');
     this.repository.emit('newFile', {name: 'file1.js'});
     assert.calledOnce(this.linter.checkFile);
